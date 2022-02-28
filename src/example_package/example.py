15a5398c21da7ce74ed14e09a167f1a79aaa0074
Kevin Albertson <kevin.eric.albertson@gmail.com>
2022-02-27 20:51:43 -0500

    first commit

---
+++ src/example_package/example.py
@@ -0,0 +1,9 @@
+def bin2hex (bin):
+    """
+    print binary as hex
+    """
+    if type(bin) != bytes:
+        raise Exception("Expected bytes")
+    for byte in bin:
+        print ("{:02x}".format(byte), end="")
+    print ()
